c648c58f7927a0e342cc899b3e5cba91c0f5541e
nicovillagra <nicovillagra123@gmail.com>
2022-11-17 17:36:56 -0300

    parte1

---
+++ main.js
@@ -0,0 +1,27 @@
+const slider = document.querySelector('.slider-contenedor');
+const sliderIndividual = document.querySelectorAll('.contenido-slider');
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
+
+window.addEventListener("resize",function(){
+    let width = sliderIndividual[0].clientWidth;
+})
+
+setInterval (function (){
+    slides()
+
+},intervalo)
+function slides(){
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform .8s";
+    contador++;
+    if(contador == sliderIndividual.length){
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.transition = "transform 0s";
+            contador=1;
+
+        },1500)
+    }
+}
